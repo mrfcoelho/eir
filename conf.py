141eaad2e346be0d5c10d7a5b354f784b77f1532
mrfcoelho <mrfcoelho@gmail.com>
2024-11-26 12:28:05 +0000

    [FEAT] upd v2

--- conf.py
+++ conf.py
@@ -27,7 +27,7 @@ locale_dirs = ['locale/']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 html_theme_options = {
